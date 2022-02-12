fc467b4088c4ed68e567dcfcb9fe3bcf2a7966a9
TimRyd <timorydholm@yahoo.at>
2022-02-12 20:33:14 +0100

    Added Character & Setup Input
    -Added Character class
    -Added MoveForward/Turn input

---
+++ Source/Course1/Course1GameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Course1GameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class COURSE1_API ACourse1GameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
